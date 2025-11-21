099e7dc8a1a5ed7629315b8ca26a88da185d49b2
MirunaIliescu21 <miruna.iliescu@gmail.com>
2025-11-21 10:58:49 +0100

    Print the restaurants - done RestaurantListPage.jsx

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        proxy: {
+            // Frontend makes request to /api/catalog/...
+            // Proxy sends it to http://localhost:8081/...
+            '/api/catalog': {
+                target: 'http://localhost:8081',
+                changeOrigin: true,
+                // /api/catalog/restaurants -> /restaurants
+                rewrite: (path) => path.replace(/^\/api\/catalog/, ''),
+            },
+        },
+    },
 })
